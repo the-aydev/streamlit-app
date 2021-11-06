589f4e67bd87df1a6934976261b41a7568f2c72d
slleekk <Ayomidedans@gmail.com>
2021-11-06 03:26:51 +0100

    earth engine

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ apps = MultiApp()
 # Add all your application here
 
 apps.add_app("Home", home.app)
-# apps.add_app("Our Work", main.app)
+# apps.add_app("Search the ocean", main.app)
 apps.add_app("Source Code", source_code.app)
 
 # The main app
